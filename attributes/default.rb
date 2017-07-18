847bbce7afe5879b28f173a04cb75ecbca7a46fc
Marky <mkurkowski@linkedin.com>
2017-07-18 09:31:30 -0700

    PD-1910 updated checksums

--- attributes/default.rb
+++ attributes/default.rb
@@ -29,12 +29,12 @@ default['nodejs']['version'] = '6.11.1'
 default['nodejs']['prefix_url']['node'] = 'https://nodejs.org/dist/'
 
 default['nodejs']['source']['url']      = nil # Auto generated
-default['nodejs']['source']['checksum'] = '9b897dd6604d50ae5fff25fd14b1c4035462d0598735799e0cfb4f17cb6e0d19'
+default['nodejs']['source']['checksum'] = '0187d4e4ef00cee2b70b0ad0689100050654f26629775d097b145d0d8727f9a0'
 
 default['nodejs']['binary']['url'] = nil # Auto generated
-default['nodejs']['binary']['checksum']['linux_x64'] = '35accd2d9ccac747eff0f236e2843bc2198ba7765e2340441d6230861bae4e1b'
-default['nodejs']['binary']['checksum']['linux_x86'] = '6721221fab4e3b3a1be6573900b9e368c7a74ac1c1c3ae982e49c5583e8962e3'
-default['nodejs']['binary']['checksum']['linux_arm64'] = '97de0340b6dbf38e3d995df880a94c58d403c3054676d8fc9192b83a3735f0b8'
+default['nodejs']['binary']['checksum']['linux_x64'] = '175e00ad504f0dca5a4d2af0f941e27ea0bd3178529fd1a9c3d67f3d75afd864'
+default['nodejs']['binary']['checksum']['linux_x86'] = '6471fbb402e2e26086e125abb6dfe51d1f1aaee4c7cc769539490de6bca635c3'
+default['nodejs']['binary']['checksum']['linux_arm64'] = 'f8c898c39ecc9806fd6b5a3b49f037fee3cfe823238b8c119b4f6f8b7869168e'
 
 default['nodejs']['make_threads'] = node['cpu'] ? node['cpu']['total'].to_i : 2
 
